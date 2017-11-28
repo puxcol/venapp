ca784fec09c12f20ff07fa5ab6356526d73a3ecb
puxcol <32552213+puxcol@users.noreply.github.com>
2017-11-28 09:54:49 -0500

    Create hail_es_json.js

---
+++ hail_es_json.js
@@ -0,0 +1,8 @@
+{ 
+  "id": "1",
+  "datetime": "1511880571",
+  "lang": "es",
+  "message": "Gracias por usar VenApp, recuerda que es con amor..!"
+  "url": "https://play.google.com/store/apps/details?id=ml.puxcol.venapp"
+  "reuse": 1
+}
