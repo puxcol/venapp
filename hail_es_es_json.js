4488e58bbee288122f389ed2be5740e8056605f6
puxcol <32552213+puxcol@users.noreply.github.com>
2017-11-28 10:55:08 -0500

    Update hail_es_es_json.js

--- hail_es_es_json.js
+++ hail_es_es_json.js
@@ -2,7 +2,7 @@
   "id": "1",
   "datetime": "1511880571",
   "lang": "es",
-  "message": "Gracias por usar VenApp, recuerda que es con amor..!"
-  "url": "https://play.google.com/store/apps/details?id=ml.puxcol.venapp"
+  "message": "Gracias por usar VenApp, recuerda que es con amor..!",
+  "url": "https://play.google.com/store/apps/details?id=ml.puxcol.venapp",
   "reuse": 1
 }
